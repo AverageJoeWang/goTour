220ed2335a2c38a481e3ada152da1abf5b854974
AverageJoeWang <oliverwang@oliverwangs-iMac-Pro.local>
2020-08-02 17:19:30 +0800

    sss

--- ch1_tour/cmd/time.go
+++ ch1_tour/cmd/time.go
@@ -1,16 +1,79 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"ch1_tour/internal/timer"
+	"github.com/spf13/cobra"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
 
-var calculateTime string
-var duration string
 
+
+const timeFormat = "2006-01-02 15:04:05"
 
 var timeCmd = &cobra.Command{
 	Use: "time",
 	Short: "时间格式处理",
 	Long: "时间格式处理",
 	Run: func(cmd *cobra.Command, args []string) {
-
 	},
+}
+
+var nowTimeCmd = &cobra.Command{
+	Use: "now",
+	Short: "时间格式处理",
+	Long: "时间格式处理",
+	Run: func(cmd *cobra.Command, args []string) {
+		nowTime := timer.GetNowTime()
+		log.Printf("输出结果: %s, %d", nowTime.Format(timeFormat),nowTime.Unix())//分别为timeformat, 第二个为unix时间戳
+	},
+}
+
+
+var calculateTime string
+var duration string
+
+
+var calculateTimeCmd = &cobra.Command{
+	Use:   "calc",
+	Short: "计算所需时间",
+	Long:  "计算所需时间",
+	Run: func(cmd *cobra.Command, args []string) {
+		var currentTimer time.Time
+		var layout = "2006-01-02 15:04:05"
+		if calculateTime == "" {
+			currentTimer = timer.GetNowTime()
+		} else {
+			var err error
+			space := strings.Count(calculateTime, " ")
+			if space == 0 {
+				layout = "2006-01-02"
+			}
+			if space == 1 {
+				layout = "2006-01-02 15:04"
+			}
+			currentTimer, err = time.Parse(layout, calculateTime)
+			if err != nil {
+				t, _ := strconv.Atoi(calculateTime)
+				currentTimer = time.Unix(int64(t), 0)
+			}
+		}
+		t, err := timer.GetCalculateTime(currentTimer, duration)
+		if err != nil {
+			log.Fatalf("timer.GetCalculateTime err: %v", err)
+		}
+
+		log.Printf("输出结果: %s, %d", t.Format(layout), t.Unix())
+	},
+}
+//go run main.go time calc -c="2006-01-02 15:04:05" -d=-2h
+//go run main.go time time
+func init() {
+	timeCmd.AddCommand(nowTimeCmd)
+	timeCmd.AddCommand(calculateTimeCmd)
+
+	calculateTimeCmd.Flags().StringVarP(&calculateTime, "calculate", "c", "", `需要计算的时间，有效单位为时间戳或已格式化后的时间`)
+	calculateTimeCmd.Flags().StringVarP(&duration, "duration", "d", "", `持续时间，有效时间单位为"ns", "us" (or "µs"), "ms", "s", "m", "h"`)
 }
--- ch1_tour/main.go
+++ ch1_tour/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"ch1_tour/cmd"
+	"log"
+)
+
 func main() {
 	err := cmd.Execute()
 	if err != nil {
