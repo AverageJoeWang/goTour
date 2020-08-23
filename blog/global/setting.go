61acfa1dcfc96771cb79c22b7de154f09fd7a6f1
AverageJoeWang <oliverwang@oliverwangs-iMac-Pro.local>
2020-08-23 19:50:38 +0800

    update Sun Aug 23 19:50:38 CST 2020

---
+++ blog/global/setting.go
@@ -0,0 +1,12 @@
+package global
+
+import "blog/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+	//EmailSetting    *setting.EmailSettingS
+	//JWTSetting      *setting.JWTSettingS
+	//Logger          *logger.Logger
+)
--- blog/main.go
+++ blog/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"blog/global"
 	"blog/internal/routers"
+	"blog/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
+
+func init()  {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init setupSetting err: %v", err)
+	}
+}
 
 func main() {
 	// r := gin.Default()
@@ -14,11 +24,52 @@ func main() {
 	// r.Run()
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:           "8081",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 	_ = s.ListenAndServe()
+}
+
+
+func setupSetting() error {
+	s, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	//err = s.ReadSection("JWT", &global.JWTSetting)
+	//if err != nil {
+	//	return err
+	//}
+	//err = s.ReadSection("Email", &global.EmailSetting)
+	//if err != nil {
+	//	return err
+	//}
+
+	global.AppSetting.DefaultContextTimeout *= time.Second
+	//global.JWTSetting.Expire *= time.Second
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	//if port != "" {
+	//	global.ServerSetting.HttpPort = port
+	//}
+	//if runMode != "" {
+	//	global.ServerSetting.RunMode = runMode
+	//}
+
+	return nil
 }
