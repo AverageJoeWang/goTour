504eb45cb9cb90041238f109c272c63c04b64c64
root <root@localhost.localdomain>
2020-08-23 19:02:02 +0800

    update Sun Aug 23 19:02:02 CST 2020

--- blog/main.go
+++ blog/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
+)
 
-func main()  {
-	r := gin.Default()
-	r.GET("/PING", func(c *gin.Context) {
-		c.JSON(200, gin.H{"mess": "pong"})
-	})
-	r.Run()
+func main() {
+	// r := gin.Default()
+	// r.GET("/PING", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{"mess": "pong"})
+	// })
+	// r.Run()
+	route := routers.NewRouters()
+	s := &http.Server{
+		Addr:           "8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
